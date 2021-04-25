a2793782101a2a9b4e5aec70fd17293dfc55a0f3
Wenke-D <Matthieu.Du@outlook.com>
2021-04-25 21:32:50 +0200

    others: comments

--- src/symbol-table.c
+++ src/symbol-table.c
@@ -1,132 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "abstract-tree.h"
 #include "symbol-table.h"
 
 
-char *StringFromType[] = {
-	"void",
-	"int",
-	"char",
-	"struct"
-};
+char *StringFromType[] = {"void", "int", "char", "struct"};
 
-STentry* symbolTable[MAXTABLES]; /* Tableau d'adresses vers les tables globales/locales */
+STentry *symbolTable[MAX_TABLES]; /* Tableau d'adresses vers les tables
+                                     globales/locales */
 
-int STmax = MAXSYMBOLS; 
-int STSize[MAXTABLES]; /* Nombres de symboles dans les tables */
-int STNumber = 0; /* Nombre de tables */
+int STmax = MAX_SYMBOLS;
+int STSize[MAX_TABLES]; /* Nombres de symboles dans each sub-table */
+int STNumber = 0;       /* Nomber of sub-tables */
 int current_type = 0;
 
-void appendST(){
-	STentry *table;
-	table = (STentry *) malloc(MAXSYMBOLS*sizeof(STentry));
-	symbolTable[STNumber] = table;
-	STSize[STNumber] = 0;
-	STNumber += 1;
+/**
+ * Append a new sub-table to symbole table
+ */
+void appendST() {
+    STentry *table;
+    table = (STentry *)malloc(MAX_SYMBOLS * sizeof(STentry));
+    symbolTable[STNumber] = table;
+    STSize[STNumber] = 0;
+    STNumber += 1;
 }
 
-void popST(){
-	symbolTable[STNumber] = NULL;
-	STSize[STNumber] = 0;
-	STNumber -= 1;
+/**
+ * Pop up current sub-table, all content will be cleared
+ */
+void popST() {
+    symbolTable[STNumber] = NULL;
+    STSize[STNumber] = 0;
+    STNumber -= 1;
 }
 
-void addVar(const char name[], int type)
-{
-<<<<<<< HEAD
-	int count;
-	for (count = 0; count < STsize; count++){
-		if (!strcmp(symbolTable[count].name, name))
-		{
-			printf("redefinition de la variable %s pres de la ligne\n", name);
-			return;
-=======
+/**
+ * Add a entry into current scope's sub-table, record its name and type
+ */
+void addVar(const char name[], int type) {
+    int i, j;
+    for (i = 0; i < STNumber; i++) {
+        for (j = 0; j < STSize[STNumber]; j++) {
+            if (!strcmp(symbolTable[i][j].name, name)) {
+                printf("Redefinition de la variable %s pres de la ligne\n",
+                       name);
+                return;
+            }
+        }
+    }
 
-	int i,j;
-	for (i = 0; i < STNumber; i++){
-		for (j = 0;j < STSize[STNumber]; j++){
-			
-			if (!strcmp(symbolTable[i][j].name, name))
-			{
-				printf("redefinition de la variable %s pres de la ligne\n", name);
-				return;
-			}
->>>>>>> 6fb22cea9ec82d805f98b5db4113475c849ec33f
-		}
-	}
+    if (STNumber == 0) {
+        appendST();
+    }
 
-	if (STNumber == 0){
-		appendST();
-	}
+    if (++(STSize[STNumber - 1]) > STmax) {
+        printf("too many variables near line \n");
+        exit(1);
+    }
 
-	if (++(STSize[STNumber-1]) > STmax)
-	{
-		printf("too many variables near line \n");
-		exit(1);
-	}
-
-	strcpy(symbolTable[STNumber-1][(STSize[STNumber-1])-1].name,name);
-	symbolTable[STNumber-1][(STSize[STNumber-1])-1].type = type;
+    strcpy(symbolTable[STNumber - 1][(STSize[STNumber - 1]) - 1].name, name);
+    symbolTable[STNumber - 1][(STSize[STNumber - 1]) - 1].type = type;
 }
 
-void createTable(Node *node){
-	while (node != NULL){
-		switch (node -> kind){
-			case VarDeclaration: addVar(node->u.identifier,current_type);break;
-			case Type: current_type = node->u.integer;break;
-			case ReturnType: current_type = node->u.integer;break;
-			case Parameter: appendST(); break;
-			case End: printTable(); popST(); break;
-			case DeclStruct: addVar(node->u.identifier,current_type); appendST(); break;
-			case Identifier: isInTable(node->u.identifier);
-			default:break;
-		}
-		
-		if (node -> firstChild != NULL)
-			createTable(node->firstChild);
+void createTable(Node *node) {
+    while (node != NULL) {
+        switch (node->kind) {
+            case Type:
+                current_type = node->u.integer;
+                break;
+            case Identifier:
+                isInTable(node->u.identifier);
+            default:
+                break;
+        }
 
-		node = node->nextSibling;
-	}
+        if (node->firstChild != NULL) {
+            createTable(node->firstChild);
+        }
+
+        node = node->nextSibling;
+    }
 }
 
-void isInTable(const char name[])
-{
-	int i,j,declared=0;
-	for (i = 0; i < STNumber; i++){
-		for (j = 0;j < STSize[i]; j++){
-			if (!strcmp(symbolTable[i][j].name, name))
-			{
-				declared = 1;
-				break;
-			}
-		}
-		if (declared == 1)
-			break;
-	}
-	
-	if (!declared)
-		printf("\x1b[31mUtilisation de la variable %s sans l'avoir déclaré\x1b[0m \n", name);
-	
+void isInTable(const char name[]) {
+    int i, j, declared = 0;
+    for (i = 0; i < STNumber; i++) {
+        for (j = 0; j < STSize[i]; j++) {
+            if (!strcmp(symbolTable[i][j].name, name)) {
+                declared = 1;
+                break;
+            }
+        }
+        if (declared == 1) break;
+    }
+
+    if (!declared)
+        printf(
+            "\x1b[31mUtilisation de la variable %s sans l'avoir déclaré\x1b[0m "
+            "\n",
+            name);
 }
 
-void printTable(){
-	int i,j;
-	printf("\x1b[34mTABLE DES SYMBOLES\n=============\x1b[0m\n");
-	for (i = 0;i < STNumber; i++){
-		if (i == 0)
-			printf("TABLE GLOBALE \n");
-		else
-			printf("TABLE LOCALE %d \n",i);
-		for (j = 0; j < STSize[i]; j++){
-			printf("%s : %s\n",
-				StringFromType[symbolTable[i][j].type],
-				symbolTable[i][j].name
-			      );
-		}
-		printf("\n");
-	}
-	printf("\n\n");
+void printTable() {
+    int i, j;
+    printf("\x1b[34mTABLE DES SYMBOLES\n=============\x1b[0m\n");
+    for (i = 0; i < STNumber; i++) {
+        if (i == 0)
+            printf("TABLE GLOBALE \n");
+        else
+            printf("TABLE LOCALE %d \n", i);
+        for (j = 0; j < STSize[i]; j++) {
+            printf("%s : %s\n", StringFromType[symbolTable[i][j].type],
+                   symbolTable[i][j].name);
+        }
+        printf("\n");
+    }
+    printf("\n\n");
 }
-
--- src/symbol-table.h
+++ src/symbol-table.h
@@ -1,13 +1,48 @@
-#define MAXSYMBOLS 256
-#define MAXNAME 64
-#define MAXTABLES 128
+// include guard
+#ifndef ST_H
+#define ST_H
 
+/* max nombre of symbols in a table of a scope */
+#define MAX_SYMBOLS 256
+/* max name length of a identifier */
+#define MAX_NAME_LENGTH 64
+/* max nomber of table */
+#define MAX_TABLES 128
+
+#define TYPE_VOID 0
+#define TYPE_INT 1
+#define TYPE_CHAR 2
+#define TYPE_STRUCT 3
+
+#include "abstract-tree.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STR_EQUAL(s1, s2) (strcpy(s1,s2) == 0)
+
+/* entry in table */
 typedef struct {
-	char name[MAXNAME];
+	char name[MAX_NAME_LENGTH];
 	int type;
 } STentry;
 
+/**
+ * Symbole table is global variable, which is list of pointer
+ * that indicates to sub-tables in each scope.
+ */
+STentry *symbolTable[MAX_TABLES];
+
+/* add an entry in the table of current scope */
 void addVar(const char name[],int type);
+
+/* print table */
 void printTable();
+
+/* create a symbole table from a AST */
 void createTable(Node *node);
+
+/* check if the name is already defined in the symbole table */
 void isInTable(const char name[]);
+
+#endif
